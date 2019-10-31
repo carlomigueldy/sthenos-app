084b05126ab4958339be792eb62e0bc908d57382
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-10-31 21:41:44 +0800

    installed Vuex and added module for auth

---
+++ src/store/modules/auth.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+const state = {
+    user: {},
+    access_token: null,
+    url: 'https://sthenos.herokuapp.com'
+}
+
+const getters = {
+    loggedIn: state => state.access_token !== null
+}
+
+const actions = {
+    async retrieveToken({ commit }, credentials) {
+        const headers = {
+            'Content-Type': 'application/json'
+        }
+        const res = await axios.post(`${state.url}/api/auth/login`, {
+            email: credentials.email,
+            password: credentials.password,
+        }, headers)
+
+        console.log(res)
+
+        commit('storeToken', res.data.access_token)
+    }
+}
+
+const mutations = {
+    storeToken: (state, access_token) => state.access_token = access_token 
+}
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations,
+}
